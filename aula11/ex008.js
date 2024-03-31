254a6d876ee9ad54b4d948e57838132dae060364
David Lopes <128656340+davidlopess@users.noreply.github.com>
2024-03-31 09:44:29 -0300

    atualizei

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+// CONDIÇÃO SIMPLES
+
+var velmax = 60
+console.log(`A velocidade do seu carro é de ${velmax}km/hr`)
+if(velmax > 60 ){
+    console.log('Vc ultrapassou a velocidade maxima, Vc será multado!')
+}
+
---
+++ aula11/ex009.js
@@ -0,0 +1,9 @@
+// CONDIÇÃO COMPOSTA
+
+var pais = 'brasil'
+if (pais == 'brasil'){
+    console.log('Vc é Brasileiro')
+
+}else{
+    console.log('VC é estrangeiro')
+}
